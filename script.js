02d6ea246791bbdb4b80b819e15f79ca24ecd043
Shivam Negi <shivam2612002@gmail.com>
2022-01-07 22:52:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let text = "";
+
+function loop(item, index) {
+  text += `<button type="button" class="btn btn-primary"><a  style="text-decoration:none; color:white" target="_blank" href="${item["url"]}">Story ${index+1}</a></button>`;
+  // text += `<button type="button" class="btn"><a  style="text-decoration:none;" target="_blank" href="${item['url']}">BUTTON ${index}</a</button> <br>`;
+}
+
+let usr_id = "";
+let url = "";
+
+function usr_i() {
+  text="";
+  let parent = document.getElementById('try');
+  let var1   = parent.getElementsByTagName('button');
+  
+  for(var i = var1.length; i--;) {
+      var1[i].parentNode.removeChild(var1[i]);
+  }
+
+  let user_name = document.getElementById("usr_name_id").value;
+  fetch(
+    "https://instagram-stories1.p.rapidapi.com/v1/get_user_id?username=" +
+      user_name,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "instagram-stories1.p.rapidapi.com",
+        "x-rapidapi-key": "a7c3efb5eemsh9a6a6f335e95d17p12ce36jsn6060446d995e",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      usr_id = data.user_id;
+      console.log(usr_id);
+
+      fetch(
+        "https://instagram-stories1.p.rapidapi.com/v1/get_stories?userid=" +
+          usr_id,
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "instagram-stories1.p.rapidapi.com",
+            "x-rapidapi-key":
+              "a7c3efb5eemsh9a6a6f335e95d17p12ce36jsn6060446d995e",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          url = data.downloadLinks;
+
+          url.forEach(loop);
+
+          document.getElementById("try").innerHTML = text;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
